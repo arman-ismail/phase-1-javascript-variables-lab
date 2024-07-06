e174ef0b753286f843943f821588e2ba83cfde40
arman ismail <armanismail544@gmail.com>
2024-07-06 09:55:50 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution in this file!
+// Cconst companyName = "Scuber";
+let employeeName = "John";
+let employeeAge = "30";
+
+let mostProfitableNeighborhood = "Chelsea";
+let companyCeo = "Susan Smith";
